6955836fee23e46c8644b33904d2c64e7ba430d2
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-07-28 18:02:45 +0300

    Add MongoDB and store polls to DB

--- index.js
+++ index.js
@@ -2,62 +2,68 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
-app.use(express.static('build'))
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
+require('dotenv').config()
+const Poll = require('./models/poll')
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const pollSchema = new mongoose.Schema({
+    question: String,
+    options: Object,
+})
 
 let polls = [
     {
         id: 1,
         question: "Your favourite programmin language?",
-        options: {1: {option: "Java", votes: 1}, 2: {option: "Javascript", votes: 3}, 3: {option: "C++", votes: 1}, 4: {option: "C", votes: 0}, 5: {option: "Python", votes: 0}}
+        options: { 1: { option: "Java", votes: 1 }, 2: { option: "Javascript", votes: 3 }, 3: { option: "C++", votes: 1 }, 4: { option: "C", votes: 0 }, 5: { option: "Python", votes: 0 } }
     },
     {
         id: 2,
         question: "How are you today?",
-        options: {1: {option: "Fine", votes: 0}, 2: {option: "Not so good", votes: 0}, 3: {option: "OK", votes: 0}, 5: {option: "Great!", votes: 0}}
+        options: { 1: { option: "Fine", votes: 0 }, 2: { option: "Not so good", votes: 0 }, 3: { option: "OK", votes: 0 }, 5: { option: "Great!", votes: 0 } }
     }
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.get('/api/polls', (request, response) => {
+    Poll.find({}).then(polls => {
+        response.json(polls)
+    })
 })
 
-app.get('/api/polls', (req, res) => {
-    res.json(polls)
+app.get('/api/polls/:id', (request, response, next) => {
+    Poll.findById(request.params.id).then(poll => {
+        if (poll) {
+            response.json(poll)
+        } else {
+            response.status(404).end()
+        }
+    })
+        .catch(error => next(error))
 })
 
-app.get('/api/polls/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const poll = polls.find(poll => poll.id === id)
-    if (poll) {
-        response.json(poll)
-    } else {
-        response.status(404).end()
-    }
+app.delete('/api/polls/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-app.delete('/api/polls/:id', (request, response) => {
-    const id = Number(request.params.id)
-    polls = polls.filter(poll => poll.id !== id)
-
-    response.status(204).end()
-})
-
-const generateId = () => {
-    const maxId = polls.length > 0
-        ? Math.max(...polls.map(n => n.id))
-        : 0
-    return maxId + 1
-}
 
 app.post('/api/polls', (request, response) => {
     const body = request.body
 
     if (!body.question) {
         return response.status(400).json({
-            error: 'content missing'
+            error: 'question missing'
         })
     }
 
@@ -67,31 +73,50 @@ app.post('/api/polls', (request, response) => {
         })
     }
 
+    const poll = new Poll({
+        question: body.question,
+        options: body.options
+    })
+
+    poll.save().then(savedPoll => {
+        response.json(savedPoll)
+    })
+})
+
+app.put('/api/polls/:id', (request, response, next) => {
+    const body = request.body
+
     const poll = {
         question: body.question,
         options: body.options,
-        date: new Date(),
-        id: generateId(),
     }
 
-    polls = polls.concat(poll)
-
-    response.json(poll)
+    Poll.findByIdAndUpdate(request.params.id, poll, { new: true })
+        .then(updatedPoll => {
+            response.json(updatedPoll)
+        })
+        .catch(error => next(error))
 })
 
-app.put('/api/polls/:id', (request, response) => {
-    const body = request.body
-    const id = Number(request.params.id)
-    const newPoll = {
-        question: body.question,
-        options: body.options,
-        id: id,
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
     }
-    polls.map((poll, i) => poll.id === id ? polls[i] = newPoll : void 0)
-    response.json(newPoll)
-})
 
-const PORT = process.env.PORT || 3001
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })

4b89a97242ae848791d05f61a54fafc951c079a0
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-08-01 22:20:36 +0300

    Deploy app

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-server.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
-
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })

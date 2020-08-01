46149103c7e305a94abaeec5467cee7c61131082
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-08-01 23:35:47 +0300

    Deploy app

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
   })
 
 app.use(history({
-  index: 'build//default.html'
+  index: 'build/index.html'
 }))
 app.use(cors())
 app.use(express.static('build'))
